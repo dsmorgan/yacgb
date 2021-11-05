7adfb1c258be6c8a29ff6c67287ffe41ab87ef02
David Morgan <dsm@nc.rr.com>
2021-11-05 04:27:41 +0000

    1st test of api responses

--- function/api.py
+++ function/api.py
@@ -7,6 +7,7 @@ from datetime import timezone
 import logging
 import os
 import json
+import re
 
 #from model.orders import Orders, orders_init
 #from yacgb.lookup import OrderBookLookup
@@ -18,13 +19,14 @@ from yacgb.util import base_symbol, quote_symbol, orderid
 from yacgb.lookup import ohlcvLookup
 from yacgb.bdt import BacktestDateTime
 from yacgb.indicators import Indicators
+from yacgb.orderslookup import OrdersGet
 
 logger=logging.getLogger()
 logger.setLevel(logging.INFO)
 
 logger.info("CCXT version: %s" % ccxt.__version__)
 #AWS parameter store usage is optional, and can be overridden with environment variables
-psconf=yacgb_aws_ps()
+psconf=yacgb_aws_ps(with_decryption=False)
 #OHLCV table lookup cache
 olcache=ohlcvLookup()
 
@@ -32,15 +34,15 @@ myexch={}
 for e in psconf.exch:
     myexch[e] = eval ('ccxt.%s ()' % e)
     myexch[e].setSandboxMode(psconf.exch_sandbox[e])
-    myexch[e].apiKey = psconf.exch_apikey[e]
-    myexch[e].secret = psconf.exch_secret[e]
-    myexch[e].password = psconf.exch_password[e]
     myexch[e].enableRateLimit = False
+    myexch[e].load_markets()
     #load_markets is required before each exchange can be used, but an expensive (time consuming) operation that can also
     # interfere with server side rate limiting. Moving this to global reduces the frequency it needs to be called.
-    myexch[e].load_markets()
 
 #orders_init()
+
+list_gbotids = re.compile('/gbotids$')
+get_gbotid = re.compile('/gbotid/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$')
 
 def lambda_handler(event, context):
     run_start = datetime.datetime.now(timezone.utc)
@@ -55,14 +57,33 @@ def lambda_handler(event, context):
         logger.info("config change, new exchange config: %s" % a)
         myexch[a] = eval ('ccxt.%s ()' % a)
         myexch[a].setSandboxMode(psconf.exch_sandbox[a])
-        myexch[a].apiKey = psconf.exch_apikey[a]
-        myexch[a].secret = psconf.exch_secret[a]
-        myexch[a].password = psconf.exch_password[a]
         myexch[a].enableRateLimit = False
         myexch[a].load_markets()
     
-    gbotids = psconf.shuffled_gbotids
+    
     response = {}
+    gbotid_match = get_gbotid.search(event["path"])
+    if gbotid_match:
+        #get details of a specific gbotid
+        response['status'] == 'ok'
+        gbotid = gbotid_match.group(1)
+        gr = GbotRunner(gbotid)
+        response.update(gr.gbot_dict)
+        #get ohlcv
+        lookup = olcache.get_candles(exchange, market_symbol, timeframe, nowbdt.dtstf(timeframe), -1440)
+        i = Indicators(lookup.aggregate('5m'))
+        response.update(i.ca_dict)
+        #get orders
+        o = OrdersGet(gbotid)
+        response.update(o.orders_dict)
+        
+    elif list_gbotids.search(event["path"]):
+        #list all gbotids (in parameter store)
+        response['status'] == 'ok'
+        response['gbotids'] = psconf.shuffled_gbotids
+    else:
+        response['status'] == 'error'
+    
     #response['gbotids'] = gbotids
     logger.info(response)
 
